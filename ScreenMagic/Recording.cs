ffac776e5ffa3381a783b54b9fbfb2da53e0ddea
Gerald Haslhofer <gerhas@microsoft.com>
2019-02-26 13:08:20 +0000

    Enable serialization of OcrResults

--- ScreenMagic/Recording.cs
+++ ScreenMagic/Recording.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using System.Drawing;
+using Newtonsoft.Json;
 
 namespace ScreenMagic
 {
@@ -15,11 +16,12 @@ namespace ScreenMagic
             return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "UniversalCopy");
         }
 
-        private static string GetUniqueFileName()
-        {
-            return DateTime.Now.Ticks.ToString() + ".jpg";
-        }
 
+        private static string GetUniqueFileNamePrefix()
+        {
+            return DateTime.Now.Ticks.ToString();
+        }
+        
         public static void EnsureWorkingDirectory()
         {
             string workingDir = GetWorkingDir();
@@ -31,17 +33,45 @@ namespace ScreenMagic
 
         public async static Task<bool> CaptureAndPersistScreenshot(Bitmap screen)
         {
-            //double scaleFactor = 1;
+            double scaleFactor = 1;
             byte[] jpegEncodedImage = Utils.SerializeBitmapToJpeg(screen);
 
-            string filename = Path.Combine(GetWorkingDir(), GetUniqueFileName());
+            string prefix = GetUniqueFileNamePrefix();
+
+            string filename = Path.Combine(GetWorkingDir(), prefix + ".jpeg");
             FileStream f = new FileStream(filename, FileMode.CreateNew);
             f.Write(jpegEncodedImage, 0, jpegEncodedImage.Count());
             f.Close();
 
-            //var results = await OcrUtils.GetOcrResults(jpegEncodedImage, scaleFactor);
+            var results = await OcrUtils.GetOcrResults(jpegEncodedImage, scaleFactor);
+            string serializePath = Path.Combine(GetWorkingDir(), prefix + ".json");
+
+            SerializeOcrResults(results, Path.Combine(serializePath));
+
+            var test = DeserializeOcrResults(serializePath);
+
             return true;
         }
+
+        public static void SerializeOcrResults(OcrResults results, string path)
+        {
+            string value = JsonConvert.SerializeObject(results);
+            StreamWriter w = new StreamWriter(path);
+            w.Write(value);
+            w.Flush();
+            w.Close();
+        }
+
+        public static OcrResults DeserializeOcrResults(string path)
+        {
+            StreamReader r = new StreamReader(path);
+            string json = r.ReadToEnd();
+            r.Close();
+
+            var result=  JsonConvert.DeserializeObject<OcrResults>(json);
+            return result;
+        }
+
     }
 
     }
